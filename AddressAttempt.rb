fc3bf726a8761f51ba07ee7c498544a6c7e95297
Garlandos <amberkgarland@gmail.com>
2017-07-20 21:03:16 +0100

    Corrections update
    Changed braces to correctly pass hash to method, added one line if return statements, changed prints to returns, corrected syntax.

--- AddressAttempt.rb
+++ AddressAttempt.rb
@@ -1,44 +1,44 @@
 def address(address_hash)
-
-    if  address_hash[:subpremise] != null
-        if  address_hash[:house_name] != null
-            if  address_hash[:house_number] != null
-                puts   address_hash[:subpremise]  + ", " +   address_hash[:house_name]  + ","
-                puts  address_hash[:house_number]  + " " +   address_hash[:street_line_1]  + ","
+    if  address_hash[:subpremise] != nil
+        if  address_hash[:house_name] != nil
+            if  address_hash[:house_number] != nil
+                return   address_hash[:subpremise]  , ", " ,   address_hash[:house_name]  , ","
+                return  address_hash[:house_number]  , " " ,   address_hash[:street_line_1]  , ","
             else
-                puts   address_hash[:subpremise]  + ", " +   address_hash[:house_name]  + ","
-              puts   address_hash[:street_line_1]  + ","
+                return   address_hash[:subpremise]  , ", " ,   address_hash[:house_name]  , ","
+                return   address_hash[:street_line_1]  , ","
             end
         else
-          if  address_hash[:house_number] != null
-            puts   address_hash[:subpremise]  + ","
-            puts   address_hash[:house_number]  + " " +   address_hash[:street_line_1]  + ","
-          end
+            if  address_hash[:house_number] != nil
+                return   address_hash[:subpremise]  , ","
+                return   address_hash[:house_number]  , " " ,   address_hash[:street_line_1]  , ","
+            end
         end
     else
-      if  address_hash[:house_name] != null
-        if  address_hash[:house_number] != null
-          puts   address_hash[:house_name]  + ","
-          puts house+number + " " +   address_hash[:street_line_1]  + ","
+        if  address_hash[:house_name] != nil
+            if  address_hash[:house_number] != nil
+                return   address_hash[:house_name]  , ","
+                return address_hash[:house_number] , " " ,   address_hash[:street_line_1]  , ","
+            else
+                return   address_hash[:house_name]  , ","
+                return   address_hash[:street_line_1]  , ","
+            end
         else
-            puts   address_hash[:house_name]  + ","
-              puts   address_hash[:street_line_1]  + ","
+            return address_hash[:house_number] , " " ,   address_hash[:street_line_1]  , ","
         end
-      else
-        puts house+number + " " +   address_hash[:street_line_1]  + ","
-      end
     end
-  if  address_hash[:street_line_2] != null
-    puts   address_hash[:street_line_2]  + ","
-  end
-  puts   address_hash[:town_or_city]  + ","
-  if  address_hash[:region] != null
-    puts   address_hash[:region]  + ","
-  end
-  puts   address_hash[:postcode] 
+    
+    return   address_hash[:street_line_2]  , "," if  address_hash[:street_line_2] != nil
+      
+    return   address_hash[:town_or_city] , "," 
+    
+    return   address_hash[:region]  , "," if address_hash[:region] != nil
+    
+    return   address_hash[:postcode] 
+    
 end
 
-address (
+address( {
   "subpremise" => "Apt 6",
   "house_number" => "22",
   "house_name" => nil,
@@ -47,4 +47,4 @@ address (
   "town_or_city" => "Sale",
   "region" => "Greater Manchester",
   "postcode" => "M33 1AX"
-)
+})
